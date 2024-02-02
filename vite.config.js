ef02570853654e55b0b4374ac6d31aaefbefb990
andy9299 <andy.tu998@gmail.com>
2024-02-02 00:35:02 -0500

    add deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/unfinity/",
+  base: "/unfinity-extras-helper/",
   plugins: [react()],
 });
