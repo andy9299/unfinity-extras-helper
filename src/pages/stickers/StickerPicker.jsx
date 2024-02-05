2a637618a2a771bca9eef40457d154c1a2846ae6
andy9299 <andy.tu998@gmail.com>
2024-02-05 13:29:28 -0500

    fix sticker-sheet image locations

--- src/pages/stickers/StickerPicker.jsx
+++ src/pages/stickers/StickerPicker.jsx
@@ -75,7 +75,7 @@ const StickerPicker = () => {
                   <img
                   key={`${sticker.id}-img`}
                   onClick={handleImageClick}
-                  src={`.../sticker-sheets/${sticker.imageName}`} 
+                  src={`../../sticker-sheets/${sticker.imageName}`} 
                   alt={sticker.name} 
                   className={`w-96 ${isSaved ? "border-8 border-green-700"	: "" }`}/>      
                 </label>
